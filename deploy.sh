09de59ba403d778ec4d49f15cea0e522213770a4
Zergity <Zergity@gmail.com>
2018-12-20 11:49:56 +0700

    Complete the script.

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,19 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 # Initialize our own variables:
 OPT_UPDATE=
+OPT_SEEDER=
 
-while getopts "h?u" opt; do
-    case "$opt" in
-    h|\?)
-        echo "$(basename ""$0"") [-h|-?] [-u]"
-        exit 0
-        ;;
+while getopts "h?us" opt; do
+	case "$opt" in
+	h|\?)
+		echo "$(basename ""$0"") [-h|-?] [-u]"
+		exit 0
+		;;
 	u)	OPT_UPDATE=1
 		;;
-    esac
+	s)	OPT_SEEDER=1
+		;;
+	esac
 done
 
 shift $((OPTIND-1))
@@ -30,42 +33,90 @@ SCP="scp -oStrictHostKeyChecking=no -o BatchMode=yes"
 PSCP="parallel-scp -OStrictHostKeyChecking=no -OBatchMode=yes"
 SSH_COPY_ID="ssh-copy-id -f"
 
+# PREPARATION
+MINER_IP=45.76.148.89
+
+if ! command -v scrypt; then
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get -yq install scrypt
+fi
+MINER_KEY=`scrypt dec ~/keys/ANxpQRnzNGhWEL3qvfn8siWsi1ai846fWr.s`
+if [ -z "$MINER_KEY" ]; then
+	>&2 echo "Mining key not provided."
+	exit -1
+fi
+
+# SEEDER
+if [[ $OPT_SEEDER ]]; then
+	DNS_SEEDER=seed.endur.io
+	SEEDER_HOST=vps.endur.io
+	SEEDER_REPO=https://github.com/endurio/ndrseeder.git
+	SEEDER_PATH=$GOPATH/src/github.com/endurio/ndrseeder
+	SEEDER_PORT=53
+
+	#$SSH $SSH_USER@$SEEDER_HOST killall -q --signal SIGINT ndrseeder
+	$SSH $SSH_USER@$SEEDER_HOST killall ndrseeder
+
+	if [[ $OPT_UPDATE ]] || ! $SSH $SSH_USER@$SEEDER_HOST stat iptables.sh \> /dev/null 2\>\&1; then
+		$SCP iptables.sh $SSH_USER@$SEEDER_HOST:./
+	fi
+	$SSH $SSH_USER@$SEEDER_HOST bash iptables.sh 22tcp ${SEEDER_PORT}udp
+
+	if [[ $OPT_UPDATE ]] || ! command -v ndrseeder; then
+		if [ ! -d "$SEEDER_PATH" ]; then
+			git clone --branch mvp $SEEDER_REPO $SEEDER_PATH
+			cd $SEEDER_PATH
+		elif [[ $OPT_UPDATE ]]; then
+			cd $SEEDER_PATH
+			git pull
+		fi
+		glide update
+		go install
+		cd -
+	fi
+
+	if [[ $OPT_UPDATE ]] || ! $SSH $SSH_USER@$SEEDER_HOST command -v ndrseeder \> /dev/null 2\>\&1; then
+		if command -v strip; then
+			strip -s "$(command -v ndrseeder)"
+		fi
+		$SCP "$(command -v ndrseeder)" $SSH_USER@$SEEDER_HOST:/usr/local/bin/
+	fi
+
+	$SSH $SSH_USER@$SEEDER_HOST "nohup ndrseeder -H$DNS_SEEDER -n$SEEDER_HOST -l:$SEEDER_PORT -s$MINER_IP >ndrseeder.log 2>&1 &"
+fi
+
 # MINER
-MINER_HOST=miner.endur.io
+MINER_REPO=git@gitlab.com:Zergity/ndrd.git
+MINER_PATH=$GOPATH/src/github.com/btcsuite/btcd
+MINER_PORT=8333
 
-# DNS SEEDER
-SEEDER_HOST=seed.endur.io
-SEEDER_NS=ns37.domaincontrol.com
-SEEDER_IP=seed.endur.io
-SEEDER_REPO=https://github.com/endurio/ndrseeder.git
-SEEDER_PATH=$GOPATH/src/github.com/endurio/ndrseeder
-SEEDER_PORT=5354
-
-if [[ $OPT_UPDATE ]] || ! $SSH $SSH_USER@$SEEDER_IP stat iptables.sh \> /dev/null 2\>\&1; then
-    $SCP iptables.sh $SSH_USER@$SEEDER_IP:./
+if [[ $OPT_UPDATE ]] || ! $SSH $SSH_USER@$MINER_IP "stat iptables.sh >/dev/null 2>&1"; then
+	$SCP iptables.sh $SSH_USER@$MINER_IP:./
 fi
-$SSH $SSH_USER@$SEEDER_IP bash iptables.sh 22tcp $SEEDER_PORT
+$SSH $SSH_USER@$MINER_IP bash iptables.sh 22tcp $MINER_PORT
 
-if [[ $OPT_UPDATE ]] || [ ! -x "$(command -v ndrseeder)" ]; then
-    if [ ! -d "$SEEDER_PATH" ]; then
-        git clone --branch mvp $SEEDER_REPO $SEEDER_PATH
-        cd $SEEDER_PATH
-    elif [[ $OPT_UPDATE ]]; then
-        cd $SEEDER_PATH
-        git pull
-    fi
-    glide update
-    go install
-    cd -
+if [[ $OPT_UPDATE ]] || ! command -v btcd; then
+	if [ ! -d "$MINER_PATH" ]; then
+		git clone --branch mvp $MINER_REPO $MINER_PATH
+		cd $MINER_PATH
+	elif [[ $OPT_UPDATE ]]; then
+		cd $MINER_PATH
+		git pull
+	fi
+	glide update
+	go install . ./cmd/...
+	cd -
 fi
 
-if [[ $OPT_UPDATE ]] || ! $SSH $SSH_USER@$SEEDER_IP command -v ndrseeder \> /dev/null 2\>\&1; then
-    if [ -x "$(command -v strip)" ]; then
-        strip -s "$(command -v ndrseeder)"
-    fi
-    $SCP "$(command -v ndrseeder)" $SSH_USER@$SEEDER_IP:/usr/local/bin/
+if [[ $OPT_UPDATE ]] || ! $SSH $SSH_USER@$MINER_IP "command -v btcd >/dev/null 2>&1"; then
+	if command -v strip; then
+		strip -s "$(command -v btcd)"
+		strip -s "$(command -v ndrctl)"
+	fi
+	$SCP "$(command -v btcd)" $SSH_USER@$MINER_IP:/usr/local/bin/
+	$SCP "$(command -v ndrctl)" $SSH_USER@$MINER_IP:/usr/local/bin/
 fi
 
-#$SSH $SSH_USER@$SEEDER_IP killall -q --signal SIGINT ndrseeder
-$SSH $SSH_USER@$SEEDER_IP killall -q ndrseeder
-$SSH $SSH_USER@$SEEDER_IP "nohup ndrseeder -H $SEEDER_HOST -n $SEEDER_NS -s $MINER_HOST >ndrseeder.log 2>&1 &"
+$SSH $SSH_USER@$MINER_IP ndrctl --rpcuser=a --rpcpass=a --skipverify stop
+sleep 3s
+$SSH $SSH_USER@$MINER_IP "nohup btcd --generate --rpcuser=a --rpcpass=a --miningkey=$MINER_KEY >btcd.log 2>&1 &"
